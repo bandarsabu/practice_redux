0b260e828122458aa4607002dd1e4894d4664e1f
bandarsabu <akhiratul2002@gmail.com>
2024-05-13 17:41:06 +0800

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import UserComponent from "./components/UserComponent";
+import CounterComponent from "./components/CounterComponent";
+
+function App() {
+  return (
+    <>
+      <CounterComponent />
+      <UserComponent />
+      <h1>NightAction - B Morning</h1>
+    </>
+  );
+}
+
+export default App;
